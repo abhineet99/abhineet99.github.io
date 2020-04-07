9b8d88593b68a3cd1ef328afc93ff17c5133148a
abhineet9 <2017csb1062@iitrpr.ac.in>
2020-04-07 22:45:46 +0530

    Updates

---
+++ precache-manifest.ce2f9e2c5804266c5bac42efa43f8ad0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386b516772f0de0d2455ebbb9ee5e489",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e45bf0bd94dab55c3f4",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "62bb52931a0c9e24d568",
+    "url": "/static/css/main.ad272647.chunk.css"
+  },
+  {
+    "revision": "8e45bf0bd94dab55c3f4",
+    "url": "/static/js/2.69bc73d1.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.69bc73d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62bb52931a0c9e24d568",
+    "url": "/static/js/main.16980868.chunk.js"
+  },
+  {
+    "revision": "8444ae0e1377649995f2",
+    "url": "/static/js/runtime-main.bc8e0b89.js"
+  },
+  {
+    "revision": "d3daa6a4617465ed2f2ecbd5e38bf35c",
+    "url": "/static/media/resume.d3daa6a4.pdf"
+  }
+]);
