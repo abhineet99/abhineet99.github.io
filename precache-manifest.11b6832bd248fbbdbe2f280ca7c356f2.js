0177b7bfed23bb9b4663d5e6f203fbeffed45fb7
abhineet9 <2017csb1062@iitrpr.ac.in>
2020-04-08 11:55:40 +0530

    Updates

---
+++ precache-manifest.11b6832bd248fbbdbe2f280ca7c356f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d6c8bfc3637153e7893eb12f822de4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0ee66eb2dadef780f63",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "f716a31dccbeced4598d",
+    "url": "/static/css/main.c2032beb.chunk.css"
+  },
+  {
+    "revision": "a0ee66eb2dadef780f63",
+    "url": "/static/js/2.4dd4b06c.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.4dd4b06c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f716a31dccbeced4598d",
+    "url": "/static/js/main.4b4dac63.chunk.js"
+  },
+  {
+    "revision": "8444ae0e1377649995f2",
+    "url": "/static/js/runtime-main.bc8e0b89.js"
+  },
+  {
+    "revision": "d3daa6a4617465ed2f2ecbd5e38bf35c",
+    "url": "/static/media/resume.d3daa6a4.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/abhineet99.github.io/precache-manifest.98290331170fc3143d7b05c02d7ea7a9.js"
+  "/precache-manifest.11b6832bd248fbbdbe2f280ca7c356f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/abhineet99.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
